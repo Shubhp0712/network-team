e8a6d267b50f17613147a2839c337317f326d6e3
Shubh Patel <160002973+Shubhp0712@users.noreply.github.com>
2026-09-24 10:00:55 +0530

    Add variable and output for public IP addresses

---
+++ main.tf
@@ -0,0 +1,9 @@
+variable "public_ips" {
+  type    = list(string)
+  default = ["10.0.0.1", "10.0.0.2", "10.0.0.3"]
+}
+
+output "public_ips_output" {
+  description = "List of public IP addresses"
+  value       = var.public_ips
+}
